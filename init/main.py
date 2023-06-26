34b8ffdf0e58e9eeecca67e1c6e6d7360daf71fd
Sergei Shevtsov <sshevczo1993@gmail.com>
2023-06-26 22:47:58 +0300

    подключили библиотеки создали первое реакцию бота на команду /start и /help

--- init/main.py
+++ init/main.py
@@ -1,11 +1,30 @@
-import requests
+import logging
+
+from aiogram import Bot, Dispatcher, executor, types
+
+API_TOKEN: str = "6184236578:AAG4cWJcP7mxym4DSv76O2wy6i_94icFutA"
 
 
-url = " http://numbersapi.com/ "
+logging.basicConfig(level=logging.INFO)
 
-req = requests.get(url)
+bot: Bot = Bot(token=API_TOKEN)
+dp: Dispatcher = Dispatcher(bot)
 
-if req.status_code == 200:
-    print(req.text)
-else:
-    print(req.status_code)
+
+@dp.message_handler(commands=["start"])
+async def procces_start_command(message: types.Message):
+    await message.answer("Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь")
+
+
+@dp.message_handler(commands=['help'])
+async def process_help_command(message: types.Message):
+    await message.answer(' Ира , привет!)')
+
+
+@dp.message_handler()
+async def send_echo(message: types.Message):
+    await message.reply(text=message.text)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
